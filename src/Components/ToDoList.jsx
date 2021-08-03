820d462c88646fc70444b7c71e88946ddc8f9d41
Miguel Rico <miguel35697@hotmail.com>
2021-08-02 22:56:32 -0500

    Completed toDo in and out animations

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { TransitionGroup } from "react-transition-group";
+import Fade from "react-reveal/Fade";
 import ToDo from "./Item";
 import AddItem from "./AddItem";
 import styles from "./ToDoList.styles.scss";
@@ -14,23 +16,35 @@ export default function ToDoList() {
       "Example to do #6",
       "Example to do #7",
       "Example to do #8",
-    ].map((item, index) => ({
+    ].map((text, index) => ({
       index,
-      item,
+      text,
     }))
   );
 
-  function addToDo(element) {
-    setToDos([...toDos, { index: toDos.length, item: element }]);
+  const [id, setId] = useState(toDos.length);
+
+  const groupProps = {
+    appear: false,
+    enter: true,
+    exit: true,
+  };
+
+  useEffect(() => {
+    console.log(toDos);
+  }, [toDos]);
+
+  function addToDo(text) {
+    setId(id + 1);
+    setToDos([...toDos, { index: id, text: text || "- -" }]);
   }
 
   function removeToDo(index) {
+    console.log("index is: " + index);
     setToDos(
-      toDos
-        .filter((item) => {
-          return index !== item.index;
-        })
-        .map((item, index) => ({ index, item: item.item }))
+      toDos.filter((item) => {
+        return index !== item.index;
+      })
     );
   }
 
@@ -39,16 +53,19 @@ export default function ToDoList() {
       <h2>To do list!</h2>
       <div className={styles.wrapper}>
         <div className={styles.toDoList}>
-          {toDos.map((item, index) => {
-            return (
-              <ToDo
-                key={index}
-                index={index}
-                stuff={item.item}
-                deleteHandler={removeToDo}
-              />
-            );
-          })}
+          <TransitionGroup {...groupProps}>
+            {toDos.map((item) => {
+              return (
+                <Fade appear={true} key={item.index} collapse right>
+                  <ToDo
+                    index={item.index}
+                    stuff={item.text}
+                    deleteHandler={removeToDo}
+                  />
+                </Fade>
+              );
+            })}
+          </TransitionGroup>
         </div>
         <AddItem addHandler={addToDo} />
       </div>
